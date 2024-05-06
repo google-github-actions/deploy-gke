8daf55669e6d27263d943688c075bdc7ce2b69d3
JeromeJu <46675578+JeromeJu@users.noreply.github.com>
2024-05-06 18:44:16 -0400

    feat: init deploy-gke Container Action (#2)
    This commit inits the deploy-gke container action. It utlizes the
    gke-deploy CLI image to deploy an existing image to a GKE cluster.
    
    The integration test, documentation and CI setup are included within
    this initial commit.
    
    <!--
    Thank you for proposing a pull request! Please note that SOME TESTS WILL
    LIKELY FAIL due to how GitHub exposes secrets in Pull Requests from
    forks.
    Someone from the team will review your Pull Request and respond.
    
    Please describe your change and any implementation details below.
    -->
    
    Signed-off-by: JeromeJu <46675578+JeromeJu@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 # Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh -l
 
 gha_version="0.1.0"
 
